4ca04e541dd4846040d7cc22a2eea9e61a9401d8
saicharan <141946357+Saicharan619@users.noreply.github.com>
2025-02-05 19:27:17 +0530

    commit 1 basics

---
+++ basics/randamresource/variablle.tf
@@ -0,0 +1,16 @@
+variable filename {
+    type = string
+    default = "sample.txt"
+  
+}
+/*variable content {
+    type = string
+    default = "PLS GIVE SOME TXT THIS IS DEFAULT"
+  
+}*/
+variable content {
+    type = map
+    default = {name ="sai",age=99,caste="indian"}
+  
+
+}
