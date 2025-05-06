e65cf81578b0292e71b65e173d287c9786a8d734
saicharan <141946357+Saicharan619@users.noreply.github.com>
2025-05-06 14:17:12 +0530

    Update main.tf

--- intance_sai/main.tf
+++ intance_sai/main.tf
@@ -7,7 +7,7 @@ provider "google" {
 resource "google_compute_instance" "terrainstancesai" {
   count = 3 
   name = "harness-vm"
- / name = "charan-${count.index + 1}"
+//name = "charan-${count.index + 1}"
   zone             = "us-central1-a"
   machine_type     = "e2-medium"
   
